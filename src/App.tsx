c372b932be1d6b2a3ed5a5e7906c345b04d88c0e
Bossortega666 <jortega@ip-depot.com.mx>
2025-06-28 10:47:14 -0600

    correccion

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useLayoutEffect} from 'react'
+import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -34,7 +34,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn morE
       </p>
     </>
   )
